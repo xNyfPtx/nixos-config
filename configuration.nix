96859847d0c7899bcfbef34acfd19090b9efba88
xNyfPtx <55147963+xNyfPtx@users.noreply.github.com>
2025-05-05 20:09:07 -0400

    commit

--- configuration.nix
+++ configuration.nix
@@ -28,6 +28,8 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   # Set your time zone.
   time.timeZone = "America/New_York";
 
